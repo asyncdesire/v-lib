7b27e1ccb407a12776533e76e7337a133d8cf8a9
Moody-Dev <huzaifaamin23@gmail.com>
2023-05-02 06:57:25 +0500

    Little modification

--- script.js
+++ script.js
@@ -13,29 +13,34 @@ function addBookToLibrary() {
     const getTitle = document.getElementById('title').value;
     const getAuthor = document.getElementById('author').value;
     const getPages = document.getElementById('pages').value;
-    // const status = readStatus();
-    // console.log(readStatus)
-    const addBook = new Book(getTitle, getAuthor, getPages, readStatus())
-    myLibrary.push(addBook);
-    // console.log(myLibrary)
+
+    if (getTitle === '' || getAuthor === '' || getPages === '') {
+        alert('Plaese fill all inputs.')
+    } else {
+        const addBook = new Book(getTitle, getAuthor, getPages, readStatus())
+        myLibrary.push(addBook);
+        clearBooks();
+    }
+    function clearBooks() {
+        getTitle = '';
+    }
 }
+
 
 function readStatus() {
     const getStatus = document.getElementById('status');
-    const getValue = getStatus.hasAttribute('checked');
-    if (getValue === true) {
+    // const getValue = getStatus.hasAttribute('checked');
+    if (getStatus.checked == true) {
         getStatus.setAttribute('value', 'On')
         return ('You have read it.')
-        // set text for true n false
     } else {
         getStatus.setAttribute('value', 'Off')
         return ("You did'nt read it yet.")
     }
 }
 
-// readStatus();
 
-function createBookCon(item) {
+function createBook(item) {
     const createCard = document.createElement('div');
     createCard.setAttribute('id', 'card');
     const createTitle = document.createElement('h1');
@@ -49,32 +54,22 @@ function createBookCon(item) {
 
     createCard.append(createTitle, createAuthor, createPages, createStatus);
     bookContainer.append(createCard);
+    // Make Book History
+    // bookHistory();
+    clearLibrary();
 }
-// console.log(createBookCon())
 
+function clearLibrary() {
+    myLibrary = [];
+}
 
 function displayBooks(e) {
-    e.preventDefault;
-    // let books;
+    e.preventDefault();
     addBookToLibrary();
     for (const book of myLibrary) {
-        createBookCon(book)
+        createBook(book)
     }
-    // for (let i = 0; i < myLibrary.length; i++) {
-    //     // console.log(bookContainer = "<div> myLibrary[i]</div>")
-    //     booksTitle = myLibrary[0];
-    //     books = myLibrary[0];
-    //     booksTitle = myLibrary[0];
-    //     console.log(books)
-    //     // bookContainer.innerHTML = myLibrary[i];
-    //     // console.log(bookContainer.innerHTML = myLibrary)
-    //     // console.log(bookContainer)/
-    // }
-    // bookContainer.innerHTML = books;
 }
 
-
-// console.log(displayBooks())
 const subBtn = document.getElementById('btn');
 subBtn.addEventListener('click', displayBooks);
-// const theHobbit = new Book('The Hobbit', "J.R.R. Tolkien", 295, 'Not read yet!');
